8ccbbdcfcf762cece8c23346f7c65bf36d0a6d37
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2025-03-01 11:42:28 -0300

    (feat) servico de pesquisador empreendedor e banco de dados

---
+++ pesquisador-empreendedor/src/main/java/com/pesquisador/empreendedor/pesquisador_empreendedor/PesquisadorEmpreendedorApplication.java
@@ -0,0 +1,13 @@
+package com.pesquisador.empreendedor.pesquisador_empreendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisadorEmpreendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisadorEmpreendedorApplication.class, args);
+	}
+
+}
---
+++ pesquisador-empreendedor/src/test/java/com/pesquisador/empreendedor/pesquisador_empreendedor/PesquisadorEmpreendedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesquisador.empreendedor.pesquisador_empreendedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisadorEmpreendedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
