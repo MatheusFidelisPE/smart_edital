d683b89708a73c8f12548d9b1bd7000427153f3d
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2025-02-28 23:20:46 -0300

    (feat) setup da aplicação utilizando postgresql e java spring

---
+++ edital-service/src/main/java/com/service/edital/edital_service/EditalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.edital.edital_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditalServiceApplication.class, args);
+	}
+
+}
---
+++ edital-service/src/test/java/com/service/edital/edital_service/EditalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.edital.edital_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
