6a4aeb45e2f4b969abdac15ee96a7a068c36e4a1
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2025-03-02 15:42:40 -0300

    (feat) Adicionando projeto que faz comunicação com llm e demais serviços para fazer as recomendações

---
+++ llmcommunication/src/main/java/com/llm/llmcommunication/LlmcommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.llm.llmcommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmcommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmcommunicationApplication.class, args);
+	}
+
+}
---
+++ llmcommunication/src/test/java/com/llm/llmcommunication/LlmcommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm.llmcommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmcommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
